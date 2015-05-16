3303dea8d1d719371d78d09c4631f087dace96cf
janicez <janicez@umbrellix.tk>
2015-05-16 21:08:02 +0100

    chghost is now unconditionally enabled

--- modules/m_chghost.c
+++ modules/m_chghost.c
@@ -195,7 +195,6 @@ static int
 mo_chghost(struct Client *client_p, struct Client *source_p,
            int parc, const char *parv[])
 {
-#ifdef ENABLE_OPER_CHGHOST
     struct Client *target_p;
 
     if(!IsOperAdmin(source_p)) {
@@ -223,11 +222,6 @@ mo_chghost(struct Client *client_p, struct Client *source_p,
     sendto_server(NULL, NULL,
                   CAP_TS6, CAP_EUID, ":%s ENCAP * CHGHOST %s :%s",
                   use_id(source_p), use_id(target_p), parv[2]);
-#else
-    sendto_one_numeric(source_p, ERR_DISABLED, form_str(ERR_DISABLED),
-                       "CHGHOST");
-#endif
-
     return 0;
 }
 
